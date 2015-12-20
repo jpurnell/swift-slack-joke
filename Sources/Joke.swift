fadb734a657434fd02f243c5ac0489cea8aa8535
Justin Purnell <jpurnell7996@gmail.com>
2015-12-19 20:24:03 -0500

    All the things.

---
+++ Sources/Joke.swift
@@ -0,0 +1,32 @@
+#if os(Linux)
+	import Glibc
+#else
+	import Darwin
+#endif
+
+// Array samle from http://stackoverflow.com/a/24101606
+extension Array {
+	func sample() -> Element {
+		let randomIndex = Int(rand()) % count
+		return self[randomIndex]
+	}
+}
+
+struct Joke {
+	let content: String
+	static let builtinJokes = [
+		"Wanted: Swift developer with 5 years experience.",
+		"Something…Taylor Swift? That's funny, right?",
+		"Swift: Still not Lisp.",
+	]
+
+init(_ content: String) {
+	self.content = content
+}
+
+// Tells one of our built in jokes
+static func tell() -> Joke {
+	let joke = Joke(Joke.builtinJokes.sample())
+	return joke
+	}
+}
---
+++ Sources/main.swift
@@ -0,0 +1,16 @@
+#if os(Linux)
+import Glibc
+#else
+import Darwin
+#endif
+import Curassow
+import Inquiline
+
+serve { request in
+	if request.path == "/joke" {
+		let joke = Joke.tell()
+		return Response(.Ok, contentType: "text/plain", body: joke.content)
+	} else {
+		return Response(.NotFound, contentType: "text/plain", body: "Not Found")
+	}
+}
